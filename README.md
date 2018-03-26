c39cb97aad20dadf5b1859fde7a29e7c53efd13f
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-03-26 20:12:58 +0530

    clean folder task added

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # About Code
-This is a Gulp built learning practise,  
+This is a Gulp built for Image Compression and Others useful tasks.
+  
 #### -- Package Built
 ` gulp  `    
 #### -- Check for existence of image or any file
 ` gulp-newer   `  
 #### -- Compress image files
 ` gulp-imagemin  `  
+#### -- Clean the folder
+` del `
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var gulp = require('gulp'),
     // only pipe throw those file not found in folder
     newer = require('gulp-newer'),
     // compress image files
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    // clean directory
+    del = require('del');
 
 // file location
 var source = 'source/',
@@ -14,6 +16,11 @@ var source = 'source/',
         in: source+'images/*.*',
         out: dest+'images/'
     };
+
+// clean
+gulp.task('clean', function(){
+    del([dest+'*']);
+});
 
 // manage images
 gulp.task('images', function () {
